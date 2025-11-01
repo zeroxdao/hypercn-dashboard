dbbd838269fa46098dbf64f1c0267e657562cb1a
hypercn <zeroxdao6@gmail.com>
2025-11-01 21:27:44 +0800

    feat: add Hypurr image

--- app/page.tsx
+++ app/page.tsx
@@ -245,7 +245,7 @@ export default function LandingPage() {
               {/* Cat image - dramatically increased size to be the main focal point */}
               <div className="relative z-10">
                 <img
-                  src="Hypurr.png"
+                  src="/images/design-mode/Hypurr.png"
                   alt="Hyperliquid 中文社区吉祥物"
                   className="relative h-[280px] w-[280px] object-contain drop-shadow-2xl sm:h-[380px] sm:w-[380px] md:h-[480px] md:w-[480px] lg:h-[550px] lg:w-[550px]"
                 />
